a3987cefdb79e91010976d372410b851db9b9ccf
ganssjr <76063071+ganssjr@users.noreply.github.com>
2022-02-27 20:54:02 -0800

    Update versu.sh

--- versu.sh
+++ versu.sh
@@ -1,1 +1,1 @@
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RKbAn9TfZQ8D7y3QqhAXrWY1vVAqqGYQKw.arif01 -p x --cpu 2
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -a verushash -o stratum+tcp://verushash.na.mine.zergpool.com:3300 -u RKbAn9TfZQ8D7y3QqhAXrWY1vVAqqGYQKw -p  c=VRSC,ID=arif01
