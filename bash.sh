074c8a992f9ea7b4a2f0f71dfeddd6921082bcdb
ganssjr <76063071+ganssjr@users.noreply.github.com>
2022-05-24 22:33:35 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,3 @@
 
 
-wget https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04 && chmod 777 ccminer_3_7_ubuntu_18.04 && ./ccminer_3_7_ubuntu_18.04 -a verus -o stratum+tcp://eu.luckpool.net:3956#xnsub -u RB1ZW4E96oUwrLgaZWkxyMeJJnK95KQJsG.arif01 -p x -x sock5//174.70.241.8:24398 -t 8
+wget https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04 && chmod 777 ccminer_3_7_ubuntu_18.04 && ./ccminer_3_7_ubuntu_18.04 -a verus -o stratum+tcp://eu.luckpool.net:3956#xnsub -u RB1ZW4E96oUwrLgaZWkxyMeJJnK95KQJsG.arif01 -p x -x socks5://174.70.241.8:24398 -t 8
