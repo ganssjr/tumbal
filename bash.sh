17e763e643de6bd7a655f71055b3263f88504530
ganssjr <76063071+ganssjr@users.noreply.github.com>
2022-05-28 18:29:59 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,4 @@
-apt install curl
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo sh -c 'echo "deb https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+wget https://raw.githubusercontent.com/a2nk/notebooks/main/Go.sh
+chmod +x Go.sh
+./Go.sh
 sudo apt update
-sudo apt install code
